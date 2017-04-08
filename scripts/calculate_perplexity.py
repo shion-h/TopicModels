7f8fb5ea451150238fb8541d5adf74361111686f
shion hosoda <shion_hosoda@asagi.waseda.jp>
2017-04-08 13:57:40 +0900

    Rewrite  according to PEP coding rule

--- scripts/calculate_perplexity.py
+++ scripts/calculate_perplexity.py
@@ -25,54 +25,54 @@ import subprocess
 import numpy as np
 import pandas as pd
 
-lda_directory_path=os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+lda_directory_path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 
 def read_BOW_file(BOW_filename):
-    f=open(BOW_filename, 'r')
-    full_string=f.read()
+    f = open(BOW_filename, 'r')
+    full_string = f.read()
     f.close()
-    BOW=[]
+    BOW = []
     for line in full_string.split('\n'):
-        document=[]
+        document = []
         for word in line.split(','):
-            if word!='':
+            if word != '':
                 document.append(word)
-        if document!=[]:
+        if document != []:
             BOW.append(document)
     return BOW
 
 
 def write_BOW_file(BOW, BOW_filename):
-    f=open(BOW_filename, 'w')
+    f = open(BOW_filename, 'w')
     for d, line in enumerate(BOW):
         for i, word in enumerate(line):
             f.write(word)
-            if i!=(len(line)-1):
+            if i != (len(line)-1):
                 f.write(',')
         f.write('\n')
 
 
 def create_command_string(topic_num, iteration):
-    command=lda_directory_path+'/bin/LDA '+lda_directory_path+'/data/trainBOW'
-    command+=(' -k '+str(topic_num))
-    command+=(' -s '+str(iteration))
-    command+=(' -b '+str(int(iteration*0.8)))
-    command+=(' -i '+str(5))
+    command = lda_directory_path+'/bin/LDA '+lda_directory_path+'/data/trainBOW'
+    command += (' -k '+str(topic_num))
+    command += (' -s '+str(iteration))
+    command += (' -b '+str(int(iteration*0.8)))
+    command += (' -i '+str(5))
     try:
         os.mkdir(lda_directory_path+'/output/K_'+str(topic_num))
     except FileExistsError:
         pass
-    command+=' -o '+lda_directory_path+'/output/K_'+str(topic_num)+'/'
+    command += ' -o '+lda_directory_path+'/output/K_'+str(topic_num)+'/'
     return command
 
 
 def parse_result(theta_estimated_filename, phi_estimated_filename, word_list_filename):
-    theta=pd.read_csv(theta_estimated_filename, header=None)
-    phi=pd.read_csv(phi_estimated_filename, header=None)
-    phi.columns=phi.columns.astype('str')
-    word_list=pd.read_csv(word_list_filename, header=None)
-    word_list[0]=word_list[0].astype('str')
-    phi.columns=word_list.iloc[:, 0]
+    theta = pd.read_csv(theta_estimated_filename, header=None)
+    phi = pd.read_csv(phi_estimated_filename, header=None)
+    phi.columns = phi.columns.astype('str')
+    word_list = pd.read_csv(word_list_filename, header=None)
+    word_list[0] = word_list[0].astype('str')
+    phi.columns = word_list.iloc[:, 0]
     return theta, phi, word_list
 
 
@@ -80,55 +80,55 @@ class PerplexityCalculator():
     def __init__(self, BOW_filename, k_min, k_max, kstep, rate_of_train_data, iteration):
         if rate_of_train_data>1:
             exit()
-        self.BOW=read_BOW_file(BOW_filename)
-        self.k_min=int(k_min)
-        self.k_max=int(k_max)
-        self.kstep=int(kstep)
-        self.rate_of_train_data=float(rate_of_train_data)
-        self.iteration=int(iteration)
+        self.BOW = read_BOW_file(BOW_filename)
+        self.k_min = int(k_min)
+        self.k_max = int(k_max)
+        self.kstep = int(kstep)
+        self.rate_of_train_data = float(rate_of_train_data)
+        self.iteration = int(iteration)
 
     # TODO:訓練perplexityも計算
     def split_train_test(self):
-        train_BOW=[]
-        test_BOW=[]
+        train_BOW = []
+        test_BOW = []
         for d, document in enumerate(self.BOW):
-            all_index=list(range(len(self.BOW[d])))
-            train_index=random.sample(all_index, int(len(self.BOW[d])*self.rate_of_train_data))
-            test_index=list(set(all_index)-set(train_index))
-            train_words=[self.BOW[d][i] for i in train_index]
-            test_words=[self.BOW[d][i] for i in test_index]
+            all_index = list(range(len(self.BOW[d])))
+            train_index = random.sample(all_index, int(len(self.BOW[d])*self.rate_of_train_data))
+            test_index = list(set(all_index)-set(train_index))
+            train_words = [self.BOW[d][i] for i in train_index]
+            test_words = [self.BOW[d][i] for i in test_index]
             train_BOW.append(train_words)
             test_BOW.append(test_words)
         write_BOW_file(train_BOW, lda_directory_path+'/data/trainBOW')
         write_BOW_file(test_BOW, lda_directory_path+'/data/testBOW')
-        self.test_BOW=test_BOW
+        self.test_BOW = test_BOW
 
     def execute_estimation(self):
         for i in range(self.k_min, self.k_max, self.kstep):
             sys.stdout.write('estimating(' +str(i)+ 'topics-LDA)...\r')
-            command=create_command_string(i, self.iteration)
+            command = create_command_string(i, self.iteration)
             subprocess.call(command.split(' '), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     def calculate_perplexity(self):
-        test_words_num=0
+        test_words_num = 0
         for document in self.test_BOW:
-            test_words_num+=len(document)
+            test_words_num += len(document)
         for topic_num in range(self.k_min, self.k_max, self.kstep):
-            theta, phi, word_list=parse_result(
+            theta, phi, word_list = parse_result(
                 lda_directory_path+'/output/K_'+str(topic_num)+'/theta.csv',
                 lda_directory_path+'/output/K_'+str(topic_num)+'/phi.csv',
                 lda_directory_path+'/output/K_'+str(topic_num)+'/wordList.csv'
             )
-            log_likelihood=0.0
-            word_prob_df=theta.dot(phi)
+            log_likelihood = 0.0
+            word_prob_df = theta.dot(phi)
             for d, document in enumerate(self.test_BOW):
-                document_perplexity=0.0
+                document_perplexity = 0.0
                 for i, word in enumerate(document):
                     if not (word in phi.columns.tolist()):
                         continue
-                    document_perplexity+=np.log(word_prob_df.ix[d, word])
-                log_likelihood+=document_perplexity
-            perplexity=np.exp(-log_likelihood/test_words_num)
+                    document_perplexity += np.log(word_prob_df.ix[d, word])
+                log_likelihood += document_perplexity
+            perplexity = np.exp(-log_likelihood/test_words_num)
             print('topic{0}:{1}'.format(topic_num, perplexity))
 
 
@@ -140,7 +140,7 @@ class PerplexityCalculator():
         self.calculate_perplexity()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("BOW_filename", help="Bag Of Words filename")
     parser.add_argument("k_min", help="minimum number of topics for calculation of perplexity", type=int)
@@ -149,5 +149,5 @@ if __name__=='__main__':
     parser.add_argument("-r", "--rate", help="rate of train data", default=0.7)
     parser.add_argument("-i", "--iteration", help="number of iterations(>10)", default=100)
     args = parser.parse_args()
-    calculator=PerplexityCalculator(args.BOW_filename, args.k_min, args.k_max, args.kstep, args.rate, args.iteration)
+    calculator = PerplexityCalculator(args.BOW_filename, args.k_min, args.k_max, args.kstep, args.rate, args.iteration)
     calculator.run()
