96f018bc95c31e873f067b46854888c30a9d25e9
shion-h <shion_hosoda@asagi.waseda.jp>
2017-05-27 00:14:06 +0900

    Delete editATM.sh

--- editATM.sh
+++
@@ -1,3 +0,0 @@
-SRC="./src/"
-INCLUDE="./src/include/"
-gvim -p ${SRC}ATM.cpp ${INCLUDE}GibbsSamplerFromATM.hpp ${SRC}GibbsSamplerFromATM.cpp ${INCLUDE}CollapsedGibbsSamplerFromATM.hpp ${SRC}CollapsedGibbsSamplerFromATM.cpp ${INCLUDE}BOWFileParser.hpp ${SRC}BOWFileParser.cpp ${INCLUDE}AuthorFileParser.hpp ${SRC}AuthorFileParser.cpp
