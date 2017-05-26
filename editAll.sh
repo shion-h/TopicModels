26cf24d98069b80a1ca1048359984c0fe4d25ada
shion-h <shion_hosoda@asagi.waseda.jp>
2017-05-27 00:13:56 +0900

    Delete editAll.sh

--- editAll.sh
+++
@@ -1,3 +0,0 @@
-SRC="./src/"
-INCLUDE="./src/include/"
-gvim -p ${SRC}LDA.cpp ${INCLUDE}GibbsSamplerFromLDA.hpp ${SRC}GibbsSamplerFromLDA.cpp ${INCLUDE}OrdinaryGibbsSamplerFromLDA.hpp ${SRC}OrdinaryGibbsSamplerFromLDA.cpp ${INCLUDE}CollapsedGibbsSamplerFromLDA.hpp ${SRC}CollapsedGibbsSamplerFromLDA.cpp ${SRC}ATM.cpp ${INCLUDE}GibbsSamplerFromATM.hpp ${SRC}GibbsSamplerFromATM.cpp ${INCLUDE}CollapsedGibbsSamplerFromATM.hpp ${SRC}CollapsedGibbsSamplerFromATM.cpp ${INCLUDE}BOWFileParser.hpp ${SRC}BOWFileParser.cpp ${INCLUDE}AuthorFileParser.hpp ${SRC}AuthorFileParser.cpp
